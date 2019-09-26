a68ded252b4b4508f69ecd4fc639311842bf16f6
flfelipelopes <fl.felipelopes288@gmail.com>
2019-09-25 22:49:38 -0300

    Jokenpo (Pedra, Papel e Tesoura): Jogo interativo. Log: * Código remodelado com lista; * Corrigido bug em que jogador sempre ganhava; * Excluído condicional de usuário; * Excluído condicional de computador; * Excluído variável de resultado; * Adicional print para resultado de partida; * Biblioteca randrange subustituída por randit.

--- ex045.py
+++ ex045.py
@@ -1,15 +1,15 @@
 #JoKenPo
 from time import sleep
-from random import randrange
-
+from random import randint
+items = ('PEDRA', 'PAPEL', 'TESOURA')
 print('\033[1m{:=^70}\033[m'.format(' JOKENPO '))
 print('')
 print('Sou profissional em JOKENPO. Será que você consegue me vencer?')
 print('')
 print('Desafio aceito. Escolha a sua opção:')
-usuario = int(input('{}[ 1 ]{} PEDRA'
-                  '\n{}[ 2 ]{} PAPEL'
-                  '\n{}[ 3 ]{} TESOURA'
+usuario = int(input('{}[ 0 ]{} PEDRA'
+                  '\n{}[ 1 ]{} PAPEL'
+                  '\n{}[ 2 ]{} TESOURA'
                   '\nQual é a sua jogada? '
                   .format('\033[1;36m', '\033[m', '\033[1;35m', '\033[m', '\033[1;33m', '\033[m')))
 sleep(1)
@@ -20,43 +20,40 @@ sleep(1)
 print('PO !!!')
 
 #Jogada Computador
-computador = randrange(1,3)
-if computador == 1:
-    computador = 'PEDRA'
-elif computador == 2:
-    computador = 'PAPEL'
-elif computador == 3:
-    computador = 'TESOURA'
-
-#Jogada Usuário
-if usuario == 1:
-    usuario = 'PEDRA'
-elif usuario == 2:
-    usuario = 'PAPEL'
-elif usuario == 3:
-    usuario = 'TESOURA'
-
-#Resultado
-if computador == 1 and usuario == 3:
-    resultado = 'JOGADOR PERDE'
-elif computador == 2 and usuario == 1:
-     resultado = 'JOGADOR PERDE'
-elif computador == 3 and usuario == 2:
-     resultado = 'JOGADOR PERDE'
-elif computador == usuario:
-    resultado = 'EMPATE'
-else:
-    resultado = 'JOGADOR VENCEU'
+computador = randint(0, 2)
 
 #Interface de Jogo
 print('')
 print('=' * 70)
-print('Computador jogou {}{}{}'.format('\033[1m', computador, '\033[m'))
-print('Jogador jogou {}{}{}'.format('\033[1m', usuario, '\033[m'))
+print('Computador jogou {}{}{}'.format('\033[1m', items[computador], '\033[m'))
+print('Jogador jogou {}{}{}'.format('\033[1m', items[usuario], '\033[m'))
 print('=' * 70)
-print('\033[1m', resultado, '\033[m')
 
-
-
-
-
+#Resultado
+if computador == 0: #compuatdor jogou PEDRA
+    if usuario == 0:
+        print('EMPATE!')
+    elif usuario == 1:
+        print('JOGADOR VENCEU')
+    elif usuario == 2:
+        print('COMPUTADOR VENCEU')
+    elif usuario > 2:
+        print('JOAGADA INVÁLIDA!')
+elif computador == 1: #compuatdor jogou PAPEL
+    if usuario == 0:
+        print('COMPUTADOR VENCEU')
+    elif usuario == 1:
+        print('EMPATE!')
+    elif usuario == 2:
+        print('JOGADOR VENCEU')
+    elif usuario > 2:
+        print('JOAGADA INVÁLIDA!')
+elif computador == 2: #compuatdor jogou TESOURA
+    if usuario == 0:
+        print('JOGADOR VENCEU')
+    elif usuario == 1:
+        print('COMPUTADOR VENCEU')
+    elif usuario == 2:
+        print('EMPATE!')
+    elif usuario > 2:
+        print('JOAGADA INVÁLIDA!')
