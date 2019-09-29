5f0f79c30fd2883752e905c8415b88b211f9ec2b
flfelipelopes <fl.felipelopes288@gmail.com>
2019-09-29 13:18:02 -0300

    #Corrigido bug em que a soma considerava todos os números da range.

--- ex048.py
+++ ex048.py
@@ -6,4 +6,3 @@ for c in range(0, 500, 3):
         soma += c
 print('A soma total de números ímpares múltiplos'
       ' de 3 entre 1 e 500 é de {}.'.format(soma))
-    
---
+++ test.py
@@ -0,0 +1,5 @@
+base = 0
+for c in range(0, 10):
+    n = int(input('Digite um número: '))
+    base += n
+print('Soma = {}'.format(base))
