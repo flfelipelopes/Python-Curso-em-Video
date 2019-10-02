a89963bfc7c2ac21b15e086dffb35e64247b5698
flfelipelopes <fl.felipelopes288@gmail.com>
2019-10-01 22:55:01 -0300

    #Leia uma frase e diga se ela é uma palíndromo ou não.

---
+++ ex053.py
@@ -0,0 +1,18 @@
+#Leia uma frase e diga se ela é uma palíndromo ou não.
+print('{:=^70}'.format('VOCÊ SABE ESCREVER UM PALÍNDROMO ?'))
+print('')
+frase = str(input('DIGITE UMA FRASE: ')).strip().upper()
+print('')
+
+palavras = frase.split()
+junto = ''.join(palavras)
+inverso = ''
+for letra in range(len(junto) -1, -1, -1):
+    inverso += junto[letra]
+
+print('O inverso de {} é {}'.format(frase, inverso))
+
+if inverso == junto:
+    print('Essa frase é um PALÍNDROMO!')
+else:
+    print('Essa frase não é um PALÍNDROMO!')
